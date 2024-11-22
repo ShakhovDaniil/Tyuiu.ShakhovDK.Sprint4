11f952b8b05ae908f1ab1edf16ec4586c116b780
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-11-23 01:29:46 +0500

    Task6

---
+++ Tyuiu.ShakhovDK.Sprint4.Task6.V12.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ShakhovDK.Sprint4.Task6.V12.Lib
+{
+    public class DataService : ISprint4Task6V12
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] res_array = Array.FindAll(array, s => s.Length == 7);
+            return res_array;
+        }
+    }
+}
---
+++ Tyuiu.ShakhovDK.Sprint4.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShakhovDK.Sprint4.Task6.V12.Lib;
+namespace Tyuiu.ShakhovDK.Sprint4.Task6.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] countries = { "Россия", "Украина", "Беларусь", "Казахстан", "Молдова", "Латвия", "Эстония" };
+            string[] res = ds.Calculate(countries);
+            string[] wait = { "Украина", "Молдова", "Эстония" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
